c0e4dd68a007b7037c6e9613e0ade00cd988b205
nikeda <4104cafeaulait@gmail.com>
2023-01-15 01:47:03 +0900

    create:webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    watch: true,
+    mode: 'production',
+    entry: {
+        main: './html/webroot/src/js/app.js',
+    },
+    output: {
+        path: path.resolve(__dirname,'html/webroot'),
+        filename: 'js/bundle.js'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/bundle.min.css',
+        }),
+        require('autoprefixer')
+    ],
+    module: {
+        rules: [
+            {
+                test: [/\.css$/,/\.scss$/],
+                // include: path.resolve(__dirname,'public/src/scss'),
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // "style-loader",
+                    "css-loader",
+                    'postcss-loader',
+                    "sass-loader"
+                ],
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin(), 
+            new CssMinimizerPlugin(),
+        ],
+    },
+}
