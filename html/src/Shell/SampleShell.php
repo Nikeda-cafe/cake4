83b3d07343bf00d33cdb3759c3eb5374333798cf
nikeda <4104cafeaulait@gmail.com>
2023-01-19 19:47:37 +0900

    finnal line

--- html/src/Shell/SampleShell.php
+++ html/src/Shell/SampleShell.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 namespace App\Shell;
 
 use Cake\Console\Shell;
@@ -8,12 +8,12 @@ class SampleShell extends Shell
 {
     use ModelAwareTrait;
     public function main()
-    {   
+    {
         $this->loadModel('Movies');
         $this->out('これはサンプルです。');
         $result = $this->Movies->insertMovieSample();
         if($result){
-            $this->out('これは成功です。');
+            $this->out('これは成功です');
         }else{
             $this->out('これは失敗です。');
         }
